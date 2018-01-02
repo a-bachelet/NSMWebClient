cef51bff3fc83ac01ff509e2d113479a54b0ac44
Alexis Bachelet <app.bachelet@gmail.com>
2018-01-02 22:00:03 +0100

    CoreModule implemented in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
+// Angular Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules Imports
+import { CoreModule } from './core/core.module';
 
+// Components Imports
 import { AppComponent } from './app.component';
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule
+  ],
+  exports: [
+
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
