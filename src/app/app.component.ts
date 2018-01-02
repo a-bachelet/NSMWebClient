7b67084516b11f7c5a4aa9a4b6484db314e0b972
Angular CLI <angular-cli@angular.io>
2018-01-02 19:54:31 +0100

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nsm-web-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nsm-web';
+}
