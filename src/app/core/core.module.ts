fd58a5dbca80409b4bdad9d2187c409450286484
Alexis Bachelet <app.bachelet@gmail.com>
2018-01-02 21:59:48 +0100

    CoreModule created

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CommonModule
+  ],
+  declarations: [
+
+  ],
+  providers: [
+
+  ],
+  entryComponents: [
+
+  ]
+})
+export class CoreModule { }
