d6f002375b862cec6098a5c51d8c0e945f25f64c
Alexis Bachelet <app.bachelet@gmail.com>
2018-01-04 19:04:03 +0100

    CORE_SERVICES constant created with RoleService implemented in it

---
+++ src/app/shared/core-services.ts
@@ -0,0 +1,9 @@
+// Angular Imports
+import { Provider } from '@angular/core';
+
+// Services Imports
+import { RoleService } from './entities/role/role.service';
+
+export const CORE_SERVICES: Provider[] = [
+  RoleService
+];
