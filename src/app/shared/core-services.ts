03c26538ab7e17f22a41315d4120f1350c13037a
Alexis Bachelet <app.bachelet@gmail.com>
2018-01-04 19:17:06 +0100

    UserService added to CORE_SERVICES constant

--- src/app/shared/core-services.ts
+++ src/app/shared/core-services.ts
@@ -3,7 +3,9 @@ import { Provider } from '@angular/core';
 
 // Services Imports
 import { RoleService } from './entities/role/role.service';
+import { UserService } from './entities/user/user.service';
 
 export const CORE_SERVICES: Provider[] = [
-  RoleService
+  RoleService,
+  UserService
 ];
