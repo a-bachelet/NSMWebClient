0c098e19e607f623b8d07743390728399c4d873f
Alexis Bachelet <app.bachelet@gmail.com>
2018-01-04 20:02:22 +0100

    AuthInterceptorService & AuthService added in CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// Services Imports
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AuthService } from './auth/auth.service';
 
 // Constants Imports
 import { CORE_SERVICES } from '../shared/core-services';
+
 
 @NgModule({
   imports: [
@@ -19,7 +25,13 @@ import { CORE_SERVICES } from '../shared/core-services';
 
   ],
   providers: [
-    ...CORE_SERVICES
+    ...CORE_SERVICES,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    },
+    AuthService
   ],
   entryComponents: [
 
