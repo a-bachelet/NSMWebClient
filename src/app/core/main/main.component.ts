62f6cd6824e9d5e9a02f7861aee7678450dd9d56
Alexis Bachelet <app.bachelet@gmail.com>
2018-01-06 15:41:42 +0100

    Main component styling

--- src/app/core/main/main.component.ts
+++ src/app/core/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {User} from '../../shared/entities/user/user';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'nsm-web-main',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  public user: User = this.ar.snapshot.data.user;
+
+  constructor(private ar: ActivatedRoute, private as: AuthService) { }
 
   ngOnInit() {
+  }
+
+  public logout() {
+    this.as.logout();
   }
 
 }
