a8d122dc7b75fe1cafbce5f1c91c79cebd51e6b3
Alexis Bachelet <app.bachelet@gmail.com>
2018-01-07 21:42:49 +0100

    UserList & RoleList finished

--- src/app/administration/roles-list/roles-list.component.ts
+++ src/app/administration/roles-list/roles-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+// Angular Imports
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+// Angular Material Imports
+import { MatTableDataSource } from '@angular/material';
+
+// Entities Imports
+import { Role } from '../../shared/entities/role/role';
+
+// Servicies Imports
+import { RoleService } from '../../shared/entities/role/role.service';
+
+// Rxjs Imports
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'nsm-web-roles-list',
   templateUrl: './roles-list.component.html',
   styleUrls: ['./roles-list.component.scss']
 })
-export class RolesListComponent implements OnInit {
+export class RolesListComponent implements OnDestroy, OnInit {
 
-  constructor() { }
+  private rolesSubscription = new Subscription();
+
+  public dataSource: MatTableDataSource<Role> = new MatTableDataSource<Role>();
+  public displayedColumns: string[] = [ '_id', 'name', 'createdAt', 'actions' ];
+
+  constructor(private rs: RoleService) { }
+
+  ngOnDestroy() {
+    this.rolesSubscription.unsubscribe();
+  }
 
   ngOnInit() {
+    this.rolesSubscription = this.rs.getAll().subscribe(res => { this.dataSource = new MatTableDataSource<Role>(res); });
   }
 
 }
--- src/app/administration/users-list/users-list.component.ts
+++ src/app/administration/users-list/users-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+// Angular Imports
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+// Angular Material Imports
+import { MatTableDataSource } from '@angular/material';
+
+// Entities Imports
+import { User } from '../../shared/entities/user/user';
+
+// Servicies Imports
+import { UserService } from '../../shared/entities/user/user.service';
+
+// Rxjs Imports
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'nsm-web-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
-export class UsersListComponent implements OnInit {
+export class UsersListComponent implements OnDestroy, OnInit {
 
-  constructor() { }
+  private usersSubscription: Subscription = new Subscription();
+
+  public dataSource: MatTableDataSource<User> = new MatTableDataSource<User>();
+  public displayedColumns: string[] = [ '_id', 'lastName', 'firstName', 'createdAt', 'actions' ];
+
+  constructor(private us: UserService) { }
+
+  ngOnDestroy() {
+    this.usersSubscription.unsubscribe();
+  }
 
   ngOnInit() {
+    this.usersSubscription = this.us.getAll().subscribe(res => { this.dataSource = new MatTableDataSource<User>(res); });
   }
 
 }
